4e512e0db0a6513fe978073e9dbe4cacfb314532
Jeff Booth <boothsemailaddress@gmail.com>
2016-06-07 16:14:05 -0700

    Initial commit

---
+++ src/com/topicplaces/android/AndroidSNS/Users/RESTLogin.java
@@ -0,0 +1,14 @@
+package com.topicplaces.android.AndroidSNS.Users;
+
+public class RESTLogin {
+
+    private String ENDPOINT;
+
+    public RESTLogin( String end ) {
+        ENDPOINT = end;
+    }
+
+    public String login(String u, String p) {
+        return "RESTLogin.login return String";
+    }
+}
